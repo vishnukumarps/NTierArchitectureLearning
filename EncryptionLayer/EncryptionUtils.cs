367c96d113c4f2b5f3af16105ab4323a8bc607dc
vishnukumarps <vishnukumar29518@gmail.com>
2020-03-24 19:54:20 +0530

    Adding Encryption layer

---
+++ EncryptionLayer/EncryptionUtils.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SecuredLoginSystem
+{
+    public class EncryptionUtils
+    {
+        public string EncryptString(string key, string plainText)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(array);
+        }
+
+        public static string DecryptString(string key, string cipherText)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        public string GenerateKey()
+        {
+           
+                int length = 32;
+
+                // creating a StringBuilder object()
+                StringBuilder str_build = new StringBuilder();
+                Random random = new Random();
+
+                char letter;
+
+                for (int i = 0; i < length; i++)
+                {
+                    double flt = random.NextDouble();
+                    int shift = Convert.ToInt32(Math.Floor(25 * flt));
+                    letter = Convert.ToChar(shift + 65);
+                    str_build.Append(letter);
+                }
+                return str_build.ToString();
+            
+        }
+        public  string GenerateKey(int iKeySize)
+        {
+            RijndaelManaged aesEncryption = new RijndaelManaged();
+            aesEncryption.KeySize = iKeySize;
+            aesEncryption.BlockSize = 128;
+            aesEncryption.Mode = CipherMode.CBC;
+            aesEncryption.Padding = PaddingMode.PKCS7;
+            aesEncryption.GenerateIV();
+            string ivStr = Convert.ToBase64String(aesEncryption.IV);
+            aesEncryption.GenerateKey();
+            string keyStr = Convert.ToBase64String(aesEncryption.Key);
+            string completeKey = ivStr + "," + keyStr;
+
+            return Convert.ToBase64String(ASCIIEncoding.UTF8.GetBytes(completeKey));
+        }
+
+        // var key = "b14ca5898a4e4133bbce2ea2315a1916";
+
+        ////Console.WriteLine("Please enter a secret key for the symmetric algorithm.");  
+        ////var key = Console.ReadLine();  
+
+        //Console.WriteLine("Please enter a string for encryption");  
+        //    var str = Console.ReadLine();
+        //var encryptedString = AesOperation.EncryptString(key, str);
+        //Console.WriteLine($"encrypted string = {encryptedString}");  
+
+        //    var decryptedString = AesOperation.DecryptString(key, encryptedString);
+        //Console.WriteLine($"decrypted string = {decryptedString}");  
+
+    }
+}
+
